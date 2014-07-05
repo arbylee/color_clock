263d965e63c9e8ee5eeecfd3107593d42036debd
Richard Lee <rblee88@gmail.com>
2014-07-05 10:05:37 -0500

    Add some time stuff

--- clock.js
+++ clock.js
@@ -8,3 +8,13 @@ var height = 960
 var svg = d3.select('#clock').append('svg')
             .attr('width', width)
             .attr('height', height)
+
+date = new Date()
+hour = date.getHours() * 5 // Standardize around 60
+minute = date.getMinutes()
+second = date.getSeconds()
+
+time_data = [hour, minute, second]
+
+svg.selectAll('.node')
+  .data(time_data)
