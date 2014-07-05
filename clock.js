b63ee707a35de3d4b11cf86771c1df0c139ba92e
Richard Lee <rblee88@gmail.com>
2014-07-05 10:03:34 -0500

    add svg element

--- clock.js
+++ clock.js
@@ -2,3 +2,9 @@ $(function(){
   console.log('hi')
 })
 
+var width = 960
+var height = 960
+
+var svg = d3.select('#clock').append('svg')
+            .attr('width', width)
+            .attr('height', height)
